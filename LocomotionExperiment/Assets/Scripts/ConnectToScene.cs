38c530f3fb82fbbffb03f0e0e41e2bb058883a30
MrAceitun0 <aceitunodev@gmail.com>
2021-04-21 08:37:32 +0200

    Dianas

---
+++ LocomotionExperiment/Assets/Scripts/ConnectToScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectToScene : MonoBehaviour
+{
+    public void connectTo(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ LocomotionExperiment/Assets/Scripts/DianaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DianaController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Diana")
+        {
+            other.GetComponent<AudioSource>().Play();
+            other.GetComponent<ParticleSystem>().Play();
+            Destroy(this.gameObject, 2f);
+        }
+    }
+}
