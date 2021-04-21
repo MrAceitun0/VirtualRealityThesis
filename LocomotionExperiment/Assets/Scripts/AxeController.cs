80a78894b248f402d3d3025cf3ad419fdaaa2770
MrAceitun0 <aceitunodev@gmail.com>
2021-04-21 23:26:51 +0200

    Wood chopping

---
+++ LocomotionExperiment/Assets/Scripts/AxeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public int health = 4;
+    public GameObject fullWood;
+    public GameObject choppedWood;
+
+    float canHit = 1.1f;
+
+    private void Update()
+    {
+        canHit += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Wood" && canHit > 1f)
+        {
+            audioSource.Play();
+            health--;
+            if(health == 0)
+            {
+                fullWood.SetActive(false);
+                choppedWood.SetActive(true);
+            }
+            canHit = 0f;
+        }
+    }
+}
