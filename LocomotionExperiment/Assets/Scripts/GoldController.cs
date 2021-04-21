3dc3c9ee3e410d67bb3a660c0299c98ba7b74875
MrAceitun0 <aceitunodev@gmail.com>
2021-04-21 22:39:55 +0200

    Gold Chest

--- LocomotionExperiment/Assets/Scripts/GoldController.cs
+++ LocomotionExperiment/Assets/Scripts/GoldController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GoldController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MeshRenderer goldA;
+    public MeshRenderer goldB;
+    public MeshRenderer goldC;
+
+    public Material opaqueMaterial;
+    public Material transparentMaterial;
+
+    string transparentName;
+
+    private void Start()
     {
-        
+        transparentName = transparentMaterial.name;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Chest")
+        {
+            if(goldA.material.name.Contains(transparentName))
+            {
+                goldA.material = opaqueMaterial;
+            }
+            else if (goldB.material.name.Contains(transparentName))
+            {
+                goldB.material = opaqueMaterial;
+            }
+            else if (goldC.material.name.Contains(transparentName))
+            {
+                goldC.material = opaqueMaterial;
+            }
+        }
+
+        other.GetComponent<AudioSource>().Play();
+        Destroy(this.gameObject);
     }
 }
