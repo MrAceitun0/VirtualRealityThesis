b58659fdd6d5f122a3edcc1ae1e683557e044451
MrAceitun0 <aceitunodev@gmail.com>
2021-04-23 00:43:05 +0200

    Full Loop Free (missing stat savings)

---
+++ LocomotionExperiment/Assets/Scripts/Confetti.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Confetti : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Confetti")
+        {
+            other.GetComponent<ParticleSystem>().Play();
+            other.GetComponent<AudioSource>().Play();
+            Destroy(other.gameObject, 2.1f);
+        }
+    }
+}
