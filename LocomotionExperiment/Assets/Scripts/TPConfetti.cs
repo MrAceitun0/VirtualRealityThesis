192dc269839632a1405155a29545585312b4e2b8
MrAceitun0 <aceitunodev@gmail.com>
2021-04-23 07:15:14 +0200

    Teleport Tutorial complete (w/o firebase)

---
+++ LocomotionExperiment/Assets/Scripts/TPConfetti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPConfetti : MonoBehaviour
+{
+    public ParticleSystem particlesA;
+    public ParticleSystem particlesB;
+    bool first = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(first && transform.position.z > 30.5f && transform.position.x > 3.5f && transform.position.x < 8f){
+            particlesA.Play();
+            particlesA.gameObject.GetComponent<AudioSource>().Play();
+            Destroy(particlesA.gameObject, 2f);
+            particlesB.Play();
+            particlesB.gameObject.GetComponent<AudioSource>().Play();
+            Destroy(particlesB.gameObject, 2f);
+
+            first = false;
+        }
+    }
+}
