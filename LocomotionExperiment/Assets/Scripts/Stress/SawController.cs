29d86d88715e47e2e756cb48d92a01cd7ad70b9d
MrAceitun0 <aceitunodev@gmail.com>
2021-04-27 08:16:36 +0200

    Grab objects in Stress Scene

---
+++ LocomotionExperiment/Assets/Scripts/Stress/SawController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawController : MonoBehaviour
+{
+    public MeshRenderer objRenderer;
+
+    public Material opaqueMat;
+
+    string opaqueName;
+
+    public AudioSource src;
+    public AudioClip coinSound;
+
+    private void Start()
+    {
+        opaqueName = opaqueMat.name;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (objRenderer.material.name.Contains(opaqueName))
+        {
+            return;
+        }
+
+        if (other.tag == "Tools" && !this.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>().isSelected)
+        {
+            objRenderer.material = opaqueMat;
+            Destroy(this.gameObject);
+            //CalmScenePacer.Instance.stage++;
+            src.PlayOneShot(coinSound);
+        }
+    }
+}
